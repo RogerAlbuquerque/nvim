4c8ff2173de6ed065b61f9a1c3dc7aaebcc7a78e
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2025-12-29 03:03:52 -0300

    updade: added new shortcuts o open and close buffers

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.keymap.set("n", "<A-S-f>", "gg=G``")
+vim.keymap.set("n", "<A-q>", "<cmd>q<cr>")
+vim.keymap.set("n", "<A-S-q>", "<cmd>q!<cr>")
+vim.keymap.set("n", "<C-s>", "<cmd>w<cr>")
 
 -- Buffers Keymaps
 vim.keymap.set("n", "<A-,>", "<cmd>BufferPrevious<cr>")
@@ -18,3 +21,6 @@ vim.keymap.set("n", "<A-6>", "<cmd>BufferGoto 6<cr>")
 vim.keymap.set("n", "<A-7>", "<cmd>BufferGoto 7<cr>")
 vim.keymap.set("n", "<A-8>", "<cmd>BufferGoto 8<cr>")
 vim.keymap.set("n", "<A-9>", "<cmd>BufferGoto 9<cr>")
+
+-- Neotree
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree left toggle<cr>")
