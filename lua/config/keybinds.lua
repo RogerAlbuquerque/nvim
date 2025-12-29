73fb91026340f5158621b8d2791dd36b59d1ce4b
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2025-12-29 01:26:35 -0300

    update: added new plugins - tab, keybinds, options and themes

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,19 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+
+-- Buffers Keymaps
+vim.keymap.set("n", "<A-,>", "<cmd>BufferPrevious<cr>")
+vim.keymap.set("n", "<A-.>", "<cmd>BufferNext<cr>")
+vim.keymap.set("n", "<A-w>", "<cmd>BufferClose<cr>")
+vim.keymap.set("n", "<A-S-w>", "<cmd>BufferRestore<cr>")
+vim.keymap.set("n", "<leader>cab", "<cmd>BufferCloseAllButCurrent<cr>")
+
+vim.keymap.set("n", "<A-1>", "<cmd>BufferGoto 1<cr>")
+vim.keymap.set("n", "<A-2>", "<cmd>BufferGoto 2<cr>")
+vim.keymap.set("n", "<A-3>", "<cmd>BufferGoto 3<cr>")
+vim.keymap.set("n", "<A-4>", "<cmd>BufferGoto 4<cr>")
+vim.keymap.set("n", "<A-5>", "<cmd>BufferGoto 5<cr>")
+vim.keymap.set("n", "<A-6>", "<cmd>BufferGoto 6<cr>")
+vim.keymap.set("n", "<A-7>", "<cmd>BufferGoto 7<cr>")
+vim.keymap.set("n", "<A-8>", "<cmd>BufferGoto 8<cr>")
+vim.keymap.set("n", "<A-9>", "<cmd>BufferGoto 9<cr>")
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,19 +1,37 @@
 local function enable_transparency()
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 end
 return {
-  {
-    "Tsuzat/NeoSolarized.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd [[ colorscheme NeoSolarized ]]
-      enable_transparency()
-    end
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {}
-  },
+    --{
+    --"Tsuzat/NeoSolarized.nvim",
+    --lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    --priority = 1000, -- make sure to load this before all the other start plugins
+    --config = function()
+    -- vim.cmd [[ colorscheme NeoSolarized ]]
+    --enable_transparency()
+    --end
+    --},
+    {
+        'sainnhe/sonokai',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            vim.opt.termguicolors = true
+            vim.g.sonokai_style = 'andromeda'
+
+            -- directly inside the plugin declaration.
+            vim.g.sonokai_enable_italic = true
+            vim.g.sonokai_disable_italic_comment = false
+            vim.g.sonokai_current_word = 'bold'
+            vim.g.sonokai_diagnostic_line_highlight = true
+            vim.cmd.colorscheme('sonokai')
+            enable_transparency()
+        end
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {}
+    },
 }
