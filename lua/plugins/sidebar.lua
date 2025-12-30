141149fcf0a321d58a98be5a37773772e64db5e7
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2025-12-29 21:39:55 -0300

    fix: show hidden files

--- lua/plugins/sidebar.lua
+++ lua/plugins/sidebar.lua
@@ -7,6 +7,17 @@ return {
             "MunifTanjim/nui.nvim",
             "nvim-tree/nvim-web-devicons", -- optional, but recommended
         },
-        lazy = false,                -- neo-tree will lazily load itself
+        lazy = false,                      -- neo-tree will lazily load itself
+        config = function()
+            require("neo-tree").setup({
+                filesystem = {
+                    filtered_items = {
+                        visible = true,
+                        hide_dotfiles = false,
+                        hide_gitignored = false,
+                    },
+                },
+            })
+        end,
     }
 }
